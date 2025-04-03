a7c089486673aa639c0db028bb92d63d6d80573b
Reige <reigejambong@gmail.com>
2025-04-04 07:48:57 +0800

    Maye last update

--- todo/urls.py
+++ todo/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the Todo API. Use /api/todos/ to interact with the API."})
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('TodoListApp.urls')), 
 ]
