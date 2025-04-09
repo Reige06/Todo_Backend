036ebde7972eac1d10bb6e46de694e5c1b214f90
Reige <reigejambong@gmail.com>
2025-04-09 12:11:00 +0800

    last

--- todo/settings.py
+++ todo/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whiteniose.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'TodoListApp',
     'rest_framework',
     
